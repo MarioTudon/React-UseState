43278253817e6ca454e6e61815d5b4ddfb41849a
Mario Tudón <mario.tudon97@gmail.com>
2024-08-29 18:38:32 -0600

    Se quito carpeta .git

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
         })
     ],
     build: {
-        outDir: resolve(__dirname, '../dist'),
+        outDir: resolve(__dirname, 'dist'),
         assetsDir: 'assets',
         minify: true,
         rollupOptions: {
